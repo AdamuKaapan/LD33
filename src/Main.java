062b2e14a839469a1587314c4bfbb2bb251d23f0
Adam Krpan <creeperinatardis@gmail.com>
2015-08-21 18:04:33 -0700

    Initial commit LD33HYPE!

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
+
+
+public class Main extends HvlTemplateInteg2D {
+
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main() {
+		super(60, 1280, 720, "LD33", new HvlDisplayModeDefault());
+	}
+
+	@Override
+	public void initialize() {
+
+	}
+
+	@Override
+	public void update(float delta) {
+
+	}
+
+}
