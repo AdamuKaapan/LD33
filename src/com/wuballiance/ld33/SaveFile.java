374ab70b3ac0c3f9f9c0106ef5eec9db9a993e81
Adam Krpan <creeperinatardis@gmail.com>
2015-08-24 14:29:55 -0700

    Updated maps.

--- src/com/wuballiance/ld33/SaveFile.java
+++ src/com/wuballiance/ld33/SaveFile.java
@@ -5,54 +5,45 @@ public class SaveFile {
 	public static boolean[] comps;
 	public static int[] shots;
 	public static boolean muted;
-	
-	static
-	{
-		names = new String[] { "FirstSteps", "Conserve", "Rounds", "HarrisMap1", "Map1", "Corners", "Loading", "HollowPoint", "StoppingForce", "OneAndOnly",
-				"Katamari", "Compass", "AliveFinally", "ChainReaction", "Clockwise"};
-		comps = new boolean[] { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
-		shots = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
+	static {
+		names = new String[] { "Maps/FirstSteps", "Maps/AliveFinally", "Maps/Conserve", "Maps/Corners", "Maps/Rounds", "Maps/HollowPoint",
+				"Maps/StoppingForce", "Maps/Loading", "Maps/Map1", "Maps/OneAndOnly", "Maps/Katamari", "Maps/Compass", "Maps/Spiral", "Maps/Zig",
+				"Maps/Clockwise", "Maps/Split", "Maps/ChainReaction", "Maps/HarrisMap1" };
+		comps = new boolean[] { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+		shots = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
 		muted = false;
 	}
-	
-	public static boolean isCompleted(String name)
-	{
+
+	public static boolean isCompleted(String name) {
 		int found = -1;
-		
-		for (int i = 0; i < names.length; i++)
-		{
-			if (names[i].equals(name))
-			{
+
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(name)) {
 				found = i;
 				break;
 			}
 		}
 		return comps[found];
 	}
-	
-	public static int getHighScore(String name)
-	{
+
+	public static int getHighScore(String name) {
 		int found = -1;
-		
-		for (int i = 0; i < names.length; i++)
-		{
-			if (names[i].equals(name))
-			{
+
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(name)) {
 				found = i;
 				break;
 			}
 		}
 		return shots[found];
 	}
-	
-	public static void setHighScore(String name, int shot)
-	{
+
+	public static void setHighScore(String name, int shot) {
 		int found = -1;
-		
-		for (int i = 0; i < names.length; i++)
-		{
-			if (names[i].equals(name))
-			{
+
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(name)) {
 				found = i;
 				break;
 			}
