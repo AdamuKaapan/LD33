2591d130320f16d37d9c915c65246a9740241fe0
Adam Krpan <creeperinatardis@gmail.com>
2015-08-24 12:41:36 -0700

    Magical things of awesome.

--- src/com/wuballiance/ld33/SaveFile.java
+++ src/com/wuballiance/ld33/SaveFile.java
@@ -9,9 +9,9 @@ public class SaveFile {
 	static
 	{
 		names = new String[] { "FirstSteps", "Conserve", "Rounds", "HarrisMap1", "Map1", "Corners", "Loading", "HollowPoint", "StoppingForce", "OneAndOnly",
-				"Katamari"};
-		comps = new boolean[] { false, false, false, false, false, false, false, false, false, false, false };
-		shots = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+				"Katamari", "Chain1"};
+		comps = new boolean[] { false, false, false, false, false, false, false, false, false, false, false, false };
+		shots = new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
 		muted = false;
 	}
 	
